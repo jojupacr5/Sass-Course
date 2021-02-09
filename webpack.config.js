461de0d34d6b8df80ba847be259875ce77f47a17
joseph <joseph.paucar@gmail.com>
2021-02-08 22:50:40 -0500

    Se creo el entorno de desarrollo, webpack configurado para leer scss files

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Sass course',
+            template: path.resolve(__dirname, 'index.html')
+        }),
+    ]
+}
